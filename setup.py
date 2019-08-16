ddb757dfd5faf6f0516aea354bf300c037a0d9d9
HanKruiger <hankruiger@gmail.com>
2019-08-16 11:25:00 +0200

    Make requirement satisfiable.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/HanKruiger/marklogic_client",
     packages=setuptools.find_packages(),
     install_requires=[
-        "requests > 2.22.0"
+        "requests >= 2.22.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
